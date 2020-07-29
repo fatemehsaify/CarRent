6ba8b0ed8bba32d576e59e0888e912847e87e35c
karimi <fatemehsaify7272@gmail.com>
2020-07-29 16:02:31 +0430

    Init project

---
+++ CarRent3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarRent3.Startup))]
+namespace CarRent3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
